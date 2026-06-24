122488f430c06b25a83adfab1b07d1ba82071e05
Chayanwahi <chayuwahi@gmail.com>
2026-06-24 19:09:46 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,18 @@
+async function loadData(){
 
+    const response = await fetch("/api/sensor-data");
+    const data = await response.json();
+
+    document.getElementById("moisture").innerText =
+        data.moisture + "%";
+
+    document.getElementById("temperature").innerText =
+        data.temperature + "°C";
+
+    document.getElementById("status").innerText =
+        data.status;
+}
+
+loadData();
+
+setInterval(loadData, 5000);
