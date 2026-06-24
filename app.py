50ccecffc90d88bccf5c4cdf44c5158bb663a0d5
Chayanwahi <chayuwahi@gmail.com>
2026-06-24 19:07:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/api/sensor-data")
+def sensor_data():
+    return jsonify({
+        "moisture": 65,
+        "temperature": 28,
+        "status": "Healthy"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
