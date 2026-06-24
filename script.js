6ac012808d571e82ef451158f6eb56651069f062
Chayanwahi <chayuwahi@gmail.com>
2026-06-24 18:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let moisture = 65;
 
+document.getElementById("moisture").innerText =
+moisture + "%";
+
+if(moisture > 50){
+    document.getElementById("status").innerText =
+    "Healthy 🌱";
+}
+else{
+    document.getElementById("status").innerText =
+    "Needs Water ⚠️";
+}
+
+document.getElementById("time").innerText =
+new Date().toLocaleString();
